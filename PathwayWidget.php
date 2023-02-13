8289913a382ad5415fab37db6df94d09ac088e3d
Egon Willighagen <egon.willighagen@gmail.com>
2023-02-13 09:12:18 +0100

    Updated (just in case)

--- PathwayWidget.php
+++ PathwayWidget.php
@@ -3,12 +3,12 @@
 <html lang="en-US">
   <meta charset="utf-8">
   <title>Redirecting&hellip;</title>
-  <link rel="canonical" href="https://new.wikipathways.org/communities/cptac.html">
-  <script>location="https://new.wikipathways.org/communities/cptac.html"</script>
-  <meta http-equiv="refresh" content="0; url=https://new.wikipathways.org/communities/cptac.html">
+  <link rel="canonical" href="https://wikipathways.org/communities/cptac.html">
+  <script>location="https://wikipathways.org/communities/cptac.html"</script>
+  <meta http-equiv="refresh" content="0; url=https://wikipathways.org/communities/cptac.html">
   <meta name="robots" content="noindex">
   <h1>Redirecting&hellip;</h1>
-  <a href="https://new.wikipathways.org/communities/cptac.html">Click here if you are not redirected.</a>
+  <a href="https://wikipathways.org/communities/cptac.html">Click here if you are not redirected.</a>
 </html>
 
 MIGHT TRY A JS REDIRECT SOLUTION TO KEEP PARAMS
